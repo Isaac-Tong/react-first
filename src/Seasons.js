d72ecf49f34b6acf9b9b02fc943d114864c0cb91
Isaac Tong <isaact943@outlook.com>
2020-08-09 18:19:07 +0800

    Started weather app

---
+++ src/Seasons.js
@@ -0,0 +1,9 @@
+const React = require('react');
+
+const Seasons = () => {
+    return (
+        <div>Hello World</div>
+    )
+}
+
+module.exports = Seasons;
--- src/index.js
+++ src/index.js
@@ -1,21 +1,29 @@
 //React and ReactDOM
 const React = require("react");
 const ReactDOM = require("react-dom");
-const Comment = require("./Comment");
-const ApprovalCard = require("./ApprovalCard");
+
 
 //React Component
-const App = () => {
-    return (
-        <div className="ui container comments">
-            <ApprovalCard>
-                <Comment name="Sam" time="Janurary 4" text="Dummy text1" />
-            </ApprovalCard>
-            <Comment name="Jack" time="Janurary 9" text="Dummy text2" />
-            <Comment name="Jane" time="Janurary 10" text="Dummy text2" />
-        </div>
-    );
-};
+// const App = () => {
+//     navigator.geolocation.getCurrentPosition(
+//         (position) => {
+//             console.log(position);
+//         },
+//         (error) => {
+//             console.log(error);
+//         }
+//     );
+//     return (
+//         <div>
+//             Hello
+//         </div>
+//     );
+// };
+class App extends React.Component {
+    render() {
+        return <div>Lattitude</div>
+    }
+}
 
 //Show react component on screen
 ReactDOM.render(<App />, document.querySelector("#root"));
