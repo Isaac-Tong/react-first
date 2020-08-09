4271c841436654c6d93485073498a6179fba0215
Isaac Tong <isaact943@outlook.com>
2020-08-09 15:38:01 +0800

    Component reusability test

--- src/Comment.js
+++ src/Comment.js
@@ -4,17 +4,19 @@ const faker = require("faker");
 const Comment = (props) => {
     return (
         <div className="comment">
-        <a href="/" className="avatar">
-            <img alt="avatar" src={faker.image.avatar()} />
-        </a>
-        <div className="content">
-            <a href="/" className="author">
-                {props.name}
+            <a href="/" className="avatar">
+                <img alt="avatar" src={faker.image.avatar()} />
             </a>
-            <div className="metadata">Posted Feburary 4th</div>
+            <div className="content">
+                <a href="/" className="author">
+                    {props.name}
+                </a>
+                <div className="metadata">
+                    <span className="date">{props.time}</span>
+                </div>
+            </div>
+            <div className="text">{props.text}</div>
         </div>
-        <div className="text">Nice blog post!</div>
-    </div>
     );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 //React and ReactDOM
 const React = require("react");
 const ReactDOM = require("react-dom");
-const Comment = require('./Comment');
+const Comment = require("./Comment");
+const ApprovalCard = require("./ApprovalCard");
 
 //React Component
 const App = () => {
     return (
         <div className="ui container comments">
-            <Comment name="Sam"/>
-            <Comment name="Jack"/>
-            <Comment name="Jane"/>
+            <ApprovalCard>
+                <Comment name="Sam" time="Janurary 4" text="Dummy text1" />
+            </ApprovalCard>
+            <Comment name="Jack" time="Janurary 9" text="Dummy text2" />
+            <Comment name="Jane" time="Janurary 10" text="Dummy text2" />
         </div>
     );
 };
